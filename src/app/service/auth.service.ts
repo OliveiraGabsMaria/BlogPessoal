ac4da8c154314d20ab107ecd83000b223dbe8c2d
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-01-27 16:39:52 -0300

    Consumo de API - Login e Cadastro

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
+import { Usuario } from '../model/Usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>('https://blogbibspessoal.herokuapp.com/usuarios/logar', userLogin)
+  }
+
+  cadastrar(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>('https://blogbibspessoal.herokuapp.com/usuario/cadastrar', usuario)
+}
+  logado(){
+    let ok: boolean = false;
+
+  if(environment.token != ''){
+    ok = true
+  }
+  return ok
+}
+}
